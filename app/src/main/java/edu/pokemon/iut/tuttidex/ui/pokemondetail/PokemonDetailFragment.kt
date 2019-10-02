bd4de9d46838cc5b70ce1a9468bfe7e317809816
Julien-Faure <julien.faure02@etu.umontpellier.fr>
2019-10-02 17:38:57 +0200

    Initial commit

---
+++ app/src/main/java/edu/pokemon/iut/tuttidex/ui/pokemondetail/PokemonDetailFragment.kt
@@ -0,0 +1,102 @@
+package edu.pokemon.iut.tuttidex.ui.pokemondetail
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.transition.TransitionInflater
+import edu.pokemon.iut.tuttidex.R
+import edu.pokemon.iut.tuttidex.databinding.FragmentPokemonDetailBinding
+import org.koin.androidx.viewmodel.ext.android.getViewModel
+import org.koin.core.parameter.parametersOf
+
+
+class PokemonDetailFragment : Fragment() {
+
+    private lateinit var viewModel: PokemonDetailViewModel
+    private lateinit var binding: FragmentPokemonDetailBinding
+
+    /*override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        postponeEnterTransition()
+        //The sharedElement between screens is only available for version above of LOLLIPOP
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            sharedElementEnterTransition = TransitionInflater.from(context).inflateTransition(android.R.transition.move)
+        }
+    }*/
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_pokemon_detail,
+            container,
+            false
+        )
+
+
+        var pokemonId = 1
+
+        //TODO 4) Recuperer l'id du pokemon contenu dans les "arguments" du fragment, enregistrer le dans la variable pokemonId
+        // Lancer l'application et vérifier que vous naviguer
+        if (arguments != null){
+            val pokeId = PokemonDetailFragmentArgs.fromBundle(arguments!!)
+            pokemonId = pokeId.pokemonId
+        }
+
+        viewModel =
+            getViewModel { parametersOf(pokemonId) }
+
+        binding.pokemonDetailViewModel = viewModel
+        binding.lifecycleOwner = this
+
+        viewModel.image.observe(this, Observer { image ->
+            //TODO 16) Lorsque l'url de l'image du pokemon est disponible la donnée sera mise à disposition ici
+            // Utiliser la pour charger l'image dans l'ImageView du layout (utiliser loadImageWithOutTransition)
+        })
+        viewModel.types.observe(this, Observer { types ->
+            //TODO 17) Plutôt que de n'afficher qu'un seul type essayer de trouver une solution pour tous les afficher cf TODO 18
+            loadTypes(types)
+        })
+
+        setHasOptionsMenu(true)
+
+        return binding.root
+    }
+
+    @SuppressLint("InflateParams")
+    private fun loadTypes(types: List<String>) {
+        //TODO 18) coder ici le todo 17, petit indice un LinearLayout peut vous aider
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.pokemon_detail_menu, menu)
+        val activity = activity
+        if (activity != null && null == getSharedIntent().resolveActivity(activity.packageManager)) {
+            menu.findItem(R.id.share)?.isVisible = false
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun shareSuccess() {
+        startActivity(getSharedIntent())
+    }
+
+    private fun getSharedIntent(): Intent {
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT, getString(R.string.number, viewModel.number.value))
+
+        return shareIntent
+    }
+}
---
+++ app/src/main/java/edu/pokemon/iut/tuttidex/ui/pokemonlist/PokemonsAdapter.kt
@@ -0,0 +1,37 @@
+package edu.pokemon.iut.tuttidex.ui.pokemonlist
+
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.DiffUtil
+import edu.pokemon.iut.tuttidex.R
+import edu.pokemon.iut.tuttidex.common.recyclerview.DataBindingAdapter
+import edu.pokemon.iut.tuttidex.common.recyclerview.DataBindingViewHolder
+import edu.pokemon.iut.tuttidex.ui.model.Pokemon
+import edu.pokemon.iut.tuttidex.ui.pokemondetail.PokemonDetailFragment
+
+class PokemonsAdapter(viewModel: PokemonListViewModel?) : DataBindingAdapter<Pokemon, PokemonListViewModel>(DiffCallback(),viewModel){
+
+    class DiffCallback : DiffUtil.ItemCallback<Pokemon>() {
+        override fun areItemsTheSame(oldItem: Pokemon, newItem: Pokemon): Boolean {
+            return oldItem.number == newItem.number
+        }
+
+        override fun areContentsTheSame(oldItem: Pokemon, newItem: Pokemon): Boolean {
+            return (oldItem.name == newItem.name
+                    && oldItem.imageUrl == newItem.imageUrl
+                    && oldItem.isCaptured == newItem.isCaptured)
+        }
+    }
+
+    override fun onBindViewHolder(holder: DataBindingViewHolder<Pokemon, PokemonListViewModel>, position: Int) {
+        super.onBindViewHolder(holder, position)
+        holder.itemView.setOnClickListener {
+            viewModel?.onPokemonClicked(getItem(holder.adapterPosition))
+            //TODO 3) Ajouter un appel au NavController pour "naviguer" vers le PokemonDetailFragment
+            // Le fragment attends l'id du pokemon que l'on veut en entrée
+            holder.itemView.findNavController().navigate(PokemonListFragmentDirections.actionPokemonListFragmentToPokemonDetailFragment(getItem(holder.adapterPosition).number))
+
+        }
+    }
+
+    override fun getItemViewType(position: Int) = R.layout.component_pokemon_line
+}
